0dc65974f0ebed1b4dbec6bf2d104aae16c7e96b
Ryan Norris (CCSU) <cisco.norris@linux.com>
2017-12-17 09:35:54 -0500

    payton: Start CarbonROM bringup.
    This commit is still very much WIP, and probably needs some corrections.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,54 +1,91 @@
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := sdm660
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+-include vendor/motorola/payton/BoardConfigVendor.mk
+
+DEVICE_PATH := device/motorola/payton
+
+BOARD_VENDOR := motorola-qcom
+
+TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno508
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := sdm660
 
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
-#CUSTOM LUN FOR USB STORAGE
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.0/file"
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
 
-#CRYPTO
-TW_INCLUDE_CRYTPO := true
+# FM
+BOARD_HAVE_QCOM_FM := true
+TARGET_QCOM_NO_FM_FIRMWARE := true
 
-#KERNEL
-TARGET_PREBUILT_KERNEL := device/motorola/payton/kernel
+# Kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := payton_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/sdm660
 BOARD_MKBOOTIMG_ARGS := --base 0x00000000 --pagesize 4096 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100   --cmdline 'console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.hab.csv=1 androidboot.hab.product=payton androidboot.hab.cid=50 buildvariant=user veritykeyid=id:b640f6ee9102b88daa3450b13ef25fc9eb143d63'  
 
-#PARTITIONS
-TARGET_USERIMAGES_USE_EXT4 := true 
+# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 268435456
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 17314086912
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 82996817920
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-#TWRP FLAGS
-TW_THEME := portrait_hdpi
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "/data"
-TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 125
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_INCLUDE_FB2PNG := true
-AB_OTA_UPDATER := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_EXCLUDE_SUPERSU := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
+# Power
+TARGET_POWERHAL_VARIANT := qcom
 
-#TESTING
+# Qualcomm Support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Radio
+BOARD_PROVIDES_LIBRIL := true
+BOARD_PROVIDES_RILD := true
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    device/motorola/payton/sepolicy
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.0/file
+
 
